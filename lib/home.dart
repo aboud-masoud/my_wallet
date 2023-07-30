0ea430e36659ffd8fea95c67d5aabb83ddaa2c9a
Abdelrahman <AbdelRahman.AlHajHussein@equiti.com>
2023-07-30 19:16:13 +0300

    handle CRUD from Firestore

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 // import 'package:hive_flutter/hive_flutter.dart';
-import 'package:my_wallet/models/amount_model.dart';
+// import 'package:my_wallet/models/amount_model.dart';
 import 'package:my_wallet/small_widgets/add_edit_record.dart';
 import 'package:my_wallet/small_widgets/header_view.dart';
 import 'package:my_wallet/small_widgets/list_cell.dart';
@@ -75,51 +75,52 @@ class _HomeScreenState extends State<HomeScreen> {
                         itemCount: snapshot.data!.docs.length,
                         itemBuilder: (ctx, index) {
                           return ListCell(
-                            type: snapshot.data!.docs[index]["type"] == "income"
-                                ? TransType.income
-                                : TransType.outcome,
+                            type: snapshot.data!.docs[index]["type"] == "income" ? TransType.income : TransType.outcome,
                             desc: snapshot.data!.docs[index]["desc"],
-                            amount: double.tryParse(snapshot
-                                    .data!.docs[index]["amount"]
-                                    .toString()) ??
-                                0,
+                            amount: double.tryParse(snapshot.data!.docs[index]["amount"].toString()) ?? 0,
                             onEdit: () {
-                              // showModalBottomSheet(
-                              //     context: context,
-                              //     isScrollControlled: true,
-                              //     builder: (context) {
-                              //       return AddEditRecord(
-                              //         onAdd: (value) async {
-                              //           if (value.amount > 0 &&
-                              //               value.desc.isNotEmpty) {
-                              //             // myList[index] = value;
-                              //             // setState(() {});
+                              showModalBottomSheet(
+                                  context: context,
+                                  isScrollControlled: true,
+                                  builder: (context) {
+                                    return AddEditRecord(
+                                      onAdd: (value) async {
+                                        if (value.amount > 0 && value.desc.isNotEmpty) {
+                                          walletDB.doc(snapshot.data!.docs[index].id).update({
+                                            "type": value.type == TransType.income ? "income" : "outcome",
+                                            "desc": value.desc,
+                                            "amount": value.amount
+                                          });
+                                          // myList[index] = value;
+                                          // setState(() {});
 
-                              //             // await myBox.putAt(index, {
-                              //             //   "type": value.type == TransType.income
-                              //             //       ? "income"
-                              //             //       : "outcome",
-                              //             //   "desc": value.desc,
-                              //             //   "amount": value.amount
-                              //             // });
-                              //             // getDataFromHive();
-                              //           } else {
-                              //             ScaffoldMessenger.of(context)
-                              //                 .showSnackBar(
-                              //               const SnackBar(
-                              //                 content: Text(
-                              //                     "Amount should be more than Zereooo and Desc should be filled"),
-                              //               ),
-                              //             );
-                              //           }
-                              //         },
-                              //         amount: myList[index].amount,
-                              //         desc: myList[index].desc,
-                              //         type: myList[index].type,
-                              //       );
-                              //     });
+                                          // await myBox.putAt(index, {
+                                          //   "type": value.type == TransType.income
+                                          //       ? "income"
+                                          //       : "outcome",
+                                          //   "desc": value.desc,
+                                          //   "amount": value.amount
+                                          // });
+                                          // getDataFromHive();
+                                        } else {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            const SnackBar(
+                                              content:
+                                                  Text("Amount should be more than Zereooo and Desc should be filled"),
+                                            ),
+                                          );
+                                        }
+                                      },
+                                      amount: snapshot.data!.docs[index]["amount"],
+                                      desc: snapshot.data!.docs[index]["desc"],
+                                      type: snapshot.data!.docs[index]["type"] == "income"
+                                          ? TransType.income
+                                          : TransType.outcome,
+                                    );
+                                  });
                             },
                             onDelete: () {
+                              walletDB.doc(snapshot.data!.docs[index].id).delete();
                               // myList.removeAt(index);
                               // setState(() {});
 
@@ -143,9 +144,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       onAdd: (value) async {
                         if (value.amount > 0 && value.desc.isNotEmpty) {
                           await walletDB.add({
-                            "type": value.type == TransType.income
-                                ? "income"
-                                : "outcome",
+                            "type": value.type == TransType.income ? "income" : "outcome",
                             "desc": value.desc,
                             "amount": value.amount
                           });
@@ -165,8 +164,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         } else {
                           ScaffoldMessenger.of(context).showSnackBar(
                             const SnackBar(
-                              content: Text(
-                                  "Amount should be more than Zereooo and Desc should be filled"),
+                              content: Text("Amount should be more than Zereooo and Desc should be filled"),
                             ),
                           );
                         }
